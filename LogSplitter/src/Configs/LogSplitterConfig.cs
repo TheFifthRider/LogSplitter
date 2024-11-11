33eabcb901d857318ec484f9f332142ffa3f4d7d
TheFifthRider <flare777@gmail.com>
2024-11-10 21:54:42 -0500

    Initial commit

---
+++ LogSplitter/src/Configs/LogSplitterConfig.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Vintagestory.API.Config;
+namespace LogSplitter.Configs
+{
+    public class LogSplitterConfig
+    {
+        public Dictionary<int, string> serverChats = new Dictionary<int, string>
+        {
+                { GlobalConstants.GeneralChatGroup, "GeneralChat" },
+                { GlobalConstants.ServerInfoChatGroup, "ServerInfo" },
+                { GlobalConstants.DamageLogChatGroup, "DamageLog" },
+                { GlobalConstants.InfoLogChatGroup, "InfoLog" },
+                { GlobalConstants.ConsoleGroup, "Console" }
+        };
+
+        public List<int> mutedChats = new List<int>{};
+
+        public string chatDirectory = GamePaths.Logs;
+
+    }
+}
---
+++ LogSplitter/src/LogSplitterModSystem.cs
@@ -0,0 +1,57 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using LogSplitter.Configs;
+using Vintagestory.API.Util;
+
+namespace LogSplitter;
+
+public class LogSplitterModSystem : ModSystem
+{
+    private static string configName = "LogSplitter.json";
+    private static LogSplitterConfig config;
+    private DateTime loggingStarted;
+
+    public void EntryAddedHandler(EnumLogType logType, string message, params object[] args)
+    {
+        if (logType == EnumLogType.Chat) {
+            int chatId = int.Parse(args[1].ToString());
+            if (!config.mutedChats.Contains(chatId)) {
+                string chatName = config.serverChats.Get(chatId) ?? $"""UnnamedChat({chatId})""";
+                string channelLogPath = Path.Combine(config.chatDirectory,  $"""{loggingStarted:yyyy-mm-dd-HHMM}_{chatName}.md""");
+                File.AppendAllLines(channelLogPath, new List<string> { $"""{DateTime.Now:yyyy.mm.dd HH:MM:ss} {args[0].ToString()}""" });
+            }
+        }
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        LoadConfig(api);
+        loggingStarted = DateTime.Now;
+        api.Logger.EntryAdded += EntryAddedHandler;
+    }
+
+    private void LoadConfig(ICoreClientAPI clientApi)
+    {
+        try
+        {
+            config = clientApi.LoadModConfig<LogSplitterConfig>(configName);
+        }
+        catch (Exception)
+        {
+            clientApi.Logger.Error("LogSplitter: Failed to load mod config!");
+            return;
+        }
+
+        if (config == null)
+        {
+            clientApi.Logger.Notification("LogSplitter: non-existant modconfig at 'ModConfig/" + configName +
+                                        "', creating default...");
+            config = new LogSplitterConfig();
+            clientApi.StoreModConfig(config, configName);
+        }
+    }
+
+}
