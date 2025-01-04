fb373dcbbb42df1be545799b099f21c6fd4c74ce
TheFifthRider <flare777@gmail.com>
2025-01-03 20:05:13 -0500

    fix: Added some more resiliency

--- LogSplitter/src/LogSplitterModSystem.cs
+++ LogSplitter/src/LogSplitterModSystem.cs
@@ -13,22 +13,35 @@ public class LogSplitterModSystem : ModSystem
     private static string configName = "LogSplitter.json";
     private static LogSplitterConfig config;
     private DateTime loggingStarted;
+    private ILogger log;
 
     public void EntryAddedHandler(EnumLogType logType, string message, params object[] args)
     {
-        if (EnumLogType.Chat == logType) {
-            int chatId = int.Parse(args[1].ToString());
-            if (!config.mutedChats.Contains(chatId)) {
-                string chatName = config.serverChats.Get(chatId) ?? $"""UnnamedChat({chatId})""";
-                string channelLogPath = Path.Combine(config.chatDirectory,  $"""{loggingStarted:yyyy-mm-dd-HHMM}_{chatName}.md""");
-                File.AppendAllLines(channelLogPath, new List<string> { $"""{DateTime.Now:yyyy.mm.dd HH:MM:ss} {args[0].ToString()}""" });
+        try {
+            if (EnumLogType.Chat == logType) {
+                if (args is null || args.Length < 2) {
+                    log.Debug("logType was of Chat, but args passed in do not look correct. Skipping message.");
+                    log.Debug($"Skipped message: (logType={logType}, message={message}, args={args})");
+                } else {
+                    string chatMessage = args[0].ToString();
+                    int chatId = int.Parse(args[1].ToString());
+                    
+                    if (!config.mutedChats.Contains(chatId)) {
+                        string chatName = config.serverChats.Get(chatId) ?? $"""UnnamedChat({chatId})""";
+                        string channelLogPath = Path.Combine(config.chatDirectory,  $"""{loggingStarted:yyyy-mm-dd-HHMM}_{chatName}.md""");
+                        File.AppendAllLines(channelLogPath, new List<string> { $"""{DateTime.Now:yyyy.mm.dd HH:MM:ss} {chatMessage}""" });
+                    }
+                }
             }
+        } catch(Exception e) {
+            log?.Error(e);
         }
     }
 
     public override void StartClientSide(ICoreClientAPI api)
     {
         LoadConfig(api);
+        log = api.Logger;
         loggingStarted = DateTime.Now;
         api.Logger.EntryAdded += EntryAddedHandler;
     }
